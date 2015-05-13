e783006b926467b53d3e46375a54bafa0ecf866a
Robert Nelson <robertcnelson@gmail.com>
2015-05-12 19:31:19 -0500

    capemgr-r10: merge to: https://github.com/torvalds/linux/commit/6c9d370c16aad97cfc6de68666634eaabac2b048
    Compare: https://github.com/torvalds/linux/compare/4cfceaf0c087f47033f5e61a801f4136d6fb68c6...6c9d370c16aad97cfc6de68666634eaabac2b048
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -14,11 +14,11 @@ toolchain="gcc_linaro_gnueabihf_4_9"
 #Kernel/Build
 KERNEL_REL=4.1
 KERNEL_TAG=${KERNEL_REL}-rc3
-BUILD=capemgr-r9
+BUILD=capemgr-r10
 
 #v3.X-rcX + upto SHA
-prev_KERNEL_SHA="a156e0682f5b328094c273435a46e32e100ba898"
-KERNEL_SHA="4cfceaf0c087f47033f5e61a801f4136d6fb68c6"
+prev_KERNEL_SHA="4cfceaf0c087f47033f5e61a801f4136d6fb68c6"
+KERNEL_SHA="6c9d370c16aad97cfc6de68666634eaabac2b048"
 
 #git branch
 BRANCH="capemgr"
